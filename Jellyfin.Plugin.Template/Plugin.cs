e9d155843e4d73ecf5a39e62a21b810174b9cd9e
Jonas Friedrich <pcjones@live.de>
2023-06-25 05:17:37 +0200

    Add test logging

--- Jellyfin.Plugin.Template/Plugin.cs
+++ Jellyfin.Plugin.Template/Plugin.cs
@@ -4,8 +4,12 @@ using System.Globalization;
 using Jellyfin.Plugin.Template.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Events;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Session;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.Logging;
 
 namespace Jellyfin.Plugin.Template;
 
@@ -14,27 +18,48 @@ namespace Jellyfin.Plugin.Template;
 /// </summary>
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
+    private readonly ILogger<Plugin> _logger;
+    private readonly ISessionManager _sessionManager;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
     /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
     /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    /// <param name="logger">Instance of the <see cref="ILogger"/> interface.</param>
+    /// <param name="sessionManager">Instance of the <see cref="ISessionManager"/> interface.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILogger<Plugin> logger, ISessionManager sessionManager)
         : base(applicationPaths, xmlSerializer)
     {
+        _logger = logger;
+        _sessionManager = sessionManager;
+
+        // Register the playback stopped handler
+        _sessionManager.PlaybackStopped += OnPlaybackStopped;
+
         Instance = this;
     }
 
     /// <inheritdoc />
-    public override string Name => "Template";
+    public override string Name => "Auto Season Download";
 
     /// <inheritdoc />
-    public override Guid Id => Guid.Parse("eb5d7894-8eef-4b36-aa6f-5d124e828ce1");
+    public override Guid Id => Guid.Parse("cf980612-95d8-44d0-be6c-8e60f659eed4");
 
     /// <summary>
     /// Gets the current plugin instance.
     /// </summary>
     public static Plugin? Instance { get; private set; }
+
+    private void OnPlaybackStopped(object? sender, PlaybackStopEventArgs e)
+    {
+        var media = e.MediaInfo;
+
+        if (media.Type == Data.Enums.BaseItemKind.Episode)
+        {
+            _logger.LogInformation("User {0} has finished watching {1}", e.Session.UserName, e.MediaInfo.Name);
+        }
+    }
 
     /// <inheritdoc />
     public IEnumerable<PluginPageInfo> GetPages()
